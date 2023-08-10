e400a715b1c0e9df0f43b3eba6b45022478c0b59
magdasoph92 <137792219+magdasoph92@users.noreply.github.com>
2023-08-10 10:21:14 +0000

    Excuse-Generator-With-JAvascript

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+import "./styles.css";
+function generateExcuse() {
+  let who = [
+    "A sneaky chinchilla",
+    "My deaf great-great-great-aunt",
+    "The naked neighbour",
+    "A wild coyote"
+  ];
+  let action = ["ate", "shredded", "crushed", "peed in"];
+  let what = ["my pyjama", "my bobby car", "my bedroom", "my laptop"];
+  let when = [
+    "before class!",
+    "while I was asleep!",
+    "during recess!",
+    "after my morning workout!"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
+window.onload = () => {
+  document.querySelector("#the-Excuse").innerHTML = generateExcuse();
 };
+
+function darkMode() {
+  let element = document.body;
+  let content = document.getElementById("DarkModetext");
+  element.className = "dark-mode";
+  content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+  let element = document.body;
+  let content = document.getElementById("DarkModetext");
+  element.className = "light-mode";
+  content.innerText = "Dark Mode is OFF";
+}
